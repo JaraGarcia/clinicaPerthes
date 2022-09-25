0d2e4c05d17b5d044127b7f5d74eeb663f463753
Jara García <jara.bilbao14@gmail.com>
2022-09-25 12:08:07 +0100

    creation

---
+++ index.js
@@ -0,0 +1,19 @@
+// if else
+// let position = window.prompt("¿En que posición has quedado? primero, segundo o tercero ");
+
+// if (position == "primero"){
+//     window.alert("¡Felicidades! Has ganado la medalla de oro");
+// } else if (position == "segundo"){
+//     window.alert("¡Felicidades! Has ganado la medalla de plata");
+// } else if(position == "tercero" ){
+//     window.alert("¡Felicidades! Has ganado la medalla de bronce");
+// } else {
+//     window.alert("Lo sentimos, vuelve a intentarlo");
+// }
+
+// //ternary
+
+// let age = window.prompt("Dime tu edad")
+// age >=18 ? window.alert("mayor de edad") : window.alert("eres menor de edad");
+
+// // && y 
